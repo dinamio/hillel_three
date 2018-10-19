e004d9be4875174e7d9ef97d973de59fa035bbbd
Dmytro Kovalenko <dkovalenko@4tifier.com>
2018-10-19 11:35:26 +0300

    initialize project

---
+++ kovalenko/home-work-01/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {2,-8,56,0,48};
+        double[] doubleArray = {2.25,-2.58,-2.59,58};
+
+        System.out.println(MathUtils.min(intArray));
+        System.out.println(MathUtils.min(doubleArray));
+    }
+}

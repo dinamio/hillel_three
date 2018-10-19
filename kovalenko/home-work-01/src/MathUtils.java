ab59636415eefff8e5289edf39e1aaa0c3d13441
Dmytro Kovalenko <dkovalenko@4tifier.com>
2018-10-19 13:04:05 +0300

    change min() method realisation

--- kovalenko/home-work-01/src/MathUtils.java
+++ kovalenko/home-work-01/src/MathUtils.java
@@ -3,7 +3,17 @@ import java.util.Arrays;
 public class MathUtils {
 
     public static int min(int[] array) {
-        return Arrays.stream(array).min().getAsInt();
+
+        int result = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+
+            result = array[i] < result
+                    ? array[i]
+                    : result;
+        }
+
+        return result;
     }
 
     public static double min(double[] array) {

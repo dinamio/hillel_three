bfdccfcef66491f1fe2ab01dfd248cea3533937c
Egor Yarmolenko <egor.w@ukr.net>
2018-11-06 23:20:01 +0200

    deleted

---
+++ yarmolenko/minArr/src/task1/Launcher.java
@@ -0,0 +1,12 @@
+package task1;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        MinimalNumber mn = new MinimalNumber();
+        int[] array = new int[]{1, -1, 0, 15, -9, 7};
+        System.out.println(mn.minArr(array));
+
+    }
+}
---
+++ yarmolenko/minArr/src/task1/MinimalNumber.java
@@ -0,0 +1,22 @@
+package task1;
+
+public class MinimalNumber {
+
+    public int minArr(int[] mas) {
+
+        int min = mas[0];
+
+        if (mas.length != 0) {
+            for(int i = 0; i < mas.length; i++) {
+                int res = mas[i];
+                if (res < min) {
+                    min = res;
+                }
+            }
+
+            return min;
+        } else {
+            throw new IllegalArgumentException("Array is empty");
+        }
+    }
+}

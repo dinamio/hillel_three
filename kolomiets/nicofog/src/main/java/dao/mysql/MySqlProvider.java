032f7ab124f8387bc154bf8be9a0e22e7a2b2d07
mikhailKolomiets <mihail.kolomiets@gmail.com>
2018-12-09 17:17:39 +0200

    make one connection from all services

--- kolomiets/nicofog/src/main/java/dao/mysql/MySqlProvider.java
+++ kolomiets/nicofog/src/main/java/dao/mysql/MySqlProvider.java
@@ -9,26 +9,24 @@ import java.sql.SQLException;
  */
 public class MySqlProvider {
 
-    private Connection connection;
+    private static Connection connection;
 
     public static final String dbUSER = "root";
     public static final String dbPASSWORD = "xxxxxxxx";
     public static final String BASEURL = "jdbc:mysql://localhost:3306/nicofog?useUnicode=yes&characterEncoding=UTF-8";
 
-    public MySqlProvider() {
+    protected static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(BASEURL, dbUSER, dbPASSWORD);
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(BASEURL, dbUSER, dbPASSWORD);
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("class not found");
-        } catch (SQLException e) {
-            System.out.println("connection error");
+            } catch (ClassNotFoundException e) {
+                System.out.println("class not found");
+            } catch (SQLException e) {
+                System.out.println("connection error");
+            }
         }
-    }
-
-    public Connection getConnection() {
         return connection;
     }
 }
